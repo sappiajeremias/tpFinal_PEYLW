f29e6b27da2fbb54b4f1700e5fa9f658dc6b06ec
sappiajeremias <jeremiassappia@gmail.com>
2022-06-21 17:09:35 -0300

    Terminada pag de posicion con js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,19 @@
-// DECLARACION DE VARIABLES
+function cambiarCopas() {
 
-let perfilIni = false;
+    let valor = document.getElementById("selectCopas");
+    valor = (valor.options[valor.selectedIndex].value);
+    let copa1;
+    let copa2;
+    copa1 = document.getElementById('copa1');
+    copa2 = document.getElementById('copa2');
+
+    if (valor == 1) {
+        copa2.setAttribute("hidden", true);
+        copa1.removeAttribute("hidden");
+    } else {
+
+        copa1.setAttribute("hidden", true);
+        copa2.removeAttribute("hidden");
+
+    }
+}
